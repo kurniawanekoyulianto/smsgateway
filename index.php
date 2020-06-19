5af2bc69205bd16f2fcf3e572ef6a02830ca4ab2
Kurniawan Eko Yulianto <kurniawanekoyulianto1997@gmail.com>
2020-06-19 22:28:16 +0700

    Commit pertama

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	// Account details s
+	$apiKey = urlencode('lf5UCZQvIOk-2xy0Dlv4l3XEVFyBNzpOLCtSaXasIZ');
+	
+	// Message details
+	$numbers = array(085884444269, 085725562882);
+	$sender = urlencode('PT. Solo Murni	');
+	$message = rawurlencode('Hello World!');
+ 
+	$numbers = implode(',', $numbers);
+ 
+	// Prepare data for POST request
+	$data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
+ 
+	// Send the POST request with cURL
+	$ch = curl_init('https://api.txtlocal.com/send/');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	// Process your response here
+	echo $response;
+?>
